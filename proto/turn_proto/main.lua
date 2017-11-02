148c69ccdce5c8198269119a3312d38fe42587f9
Christopher Lif <chrislnk12@gmail.com>
2017-11-02 15:15:20 -0500

    battle proto setup
    This will be the prototype for how the battle scenes will work

---
+++ proto/turn_proto/main.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+local composer = require "composer"
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene("scn.btl")
